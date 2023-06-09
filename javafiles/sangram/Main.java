b45c71ade0d970252f90ff8feb6323031f027bc6
sangramsingh <banty123a.bksc@outlook.com>
2023-06-09 23:18:40 +0530

    changes

--- javafiles/sangram/Main.java
+++ javafiles/sangram/Main.java
@@ -1,69 +1,14 @@
-package kskkkkskkks
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- nackage javafiles.sangram;
+package javafiles.sangram;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+    
         System.out.println("Hello Sangram");
         System.out.println("Namaskar Sangram");
-    }
+  }
 }
+
+
+
+
