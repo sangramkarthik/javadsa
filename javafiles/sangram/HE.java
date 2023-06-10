07c51a5c1f8461f93137eb3d7ad46126657dd96c
sangramsingh <banty123a.bksc@outlook.com>
2023-06-10 22:38:58 +0530

    Input Program

---
+++ javafiles/sangram/HE.java
@@ -0,0 +1,11 @@
+package javafiles.sangram;
+
+import java.util.Scanner;
+
+public class HE {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner inout = new Scanner(System.in);
+        System.out.println(inout.nextInt());
+    }
+}
