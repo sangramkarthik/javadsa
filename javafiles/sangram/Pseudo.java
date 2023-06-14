297c7ea34721f327a9ec17cc84d364b5e7d51372
sangramsingh <banty123a.bksc@outlook.com>
2023-06-14 22:58:08 +0530

    New test program added

---
+++ javafiles/sangram/Pseudo.java
@@ -0,0 +1,19 @@
+package javafiles.sangram;
+
+import java.util.Scanner;
+
+public class Pseudo {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        System.out.println(name.nextLine());
+        Scanner rollno = new Scanner(System.in);
+        System.out.println("Enter your roll. no. :");
+        System.out.println(rollno.nextInt());
+
+
+
+
+
+    }
+}
