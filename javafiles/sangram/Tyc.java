3359c10906b31688b2388fa5eb676ab2af76cd81
sangramsingh <banty123a.bksc@outlook.com>
2023-06-17 23:28:39 +0530

    Tyc.java file added for Java Type-Casting/Conversion

---
+++ javafiles/sangram/Tyc.java
@@ -0,0 +1,11 @@
+package javafiles.sangram;
+
+import java.util.Scanner;
+
+public class Tyc {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
+    }
+}
