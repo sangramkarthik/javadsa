ae74159f936425dd31789c124466d88af58d19fd
sangramsingh <banty123a.bksc@outlook.com>
2023-06-18 16:41:36 +0530

    changes made

---
+++ javafiles/sangram/Practise.java
@@ -0,0 +1,19 @@
+package javafiles.sangram;
+
+public class Practise {
+    public static void main(String[] args) {
+//        byte a , b , c,d;
+//        a=90;
+//        b=56;
+//        c=99;
+//
+//        d= a*b/c;
+//        System.out.println(d); //automatic Type promotion in expression;
+//          byte b=60;
+//          b=(byte)(b*2);
+//        int n='A';
+//        System.out.println(n);
+        System.out.println(8+90);
+
+    }
+}
---
+++ javafiles/sangram/add.java
@@ -0,0 +1,14 @@
+package javafiles.sangram;
+
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        float num1,num2,add;
+        num2=input.nextFloat();
+        num1=input.nextFloat();
+        add=num1+num2;
+        System.out.println(add);
+    }
+}
