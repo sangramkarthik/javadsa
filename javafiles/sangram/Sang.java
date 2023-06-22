6480579d6b546ee3d27fd869b29ca57a24c196a3
sangramsingh <banty123a.bksc@outlook.com>
2023-06-22 22:47:06 +0530

    Sang.java file added

---
+++ javafiles/sangram/Sang.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sang {
+  public static void main(String[] args) {
+    System.out.println("hello");
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your Name: ");
+    String Name = input.nextLine();
+    System.out.println("Your Name is "+Name);
+
+  }
+}
