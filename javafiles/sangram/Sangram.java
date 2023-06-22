617c2a5553a0c3b75510c56b8f836cf99df8c21e
sangramsingh <banty123a.bksc@outlook.com>
2023-06-22 22:47:39 +0530

    Snagram.java file added

---
+++ javafiles/sangram/Sangram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sangram {
+  public static void main(String[] args) {
+    System.out.println("Program Start from Here");
+    
+    byte a = 40;
+    byte c=89;
+    byte b=90;
+    int d =a*b/c;
+    System.out.println(d);
+  }
+}
+
