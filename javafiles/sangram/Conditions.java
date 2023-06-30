a277d26ecf0a4986c7d2c481c407e2ac00897f6a
sangramsingh <banty123a.bksc@outlook.com>
2023-06-30 13:27:01 +0530

    files deleted to change layout

--- javafiles/sangram/Conditions.java
+++
@@ -1,5 +0,0 @@
-public class Conditions {
-  public static void main(String[] args) {
-    System.out.println("hello,Sangram!");
-  }
-}
--- javafiles/sangram/Main.java
+++
@@ -1,8 +0,0 @@
-package javafiles.sangram;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Sangram");
-        System.out.println("Namaskar Sangram");
-    }
-}
