6b5ff737220c35485466b10e7bad26cf003f2657
sangramsingh <banty123a.bksc@outlook.com>
2023-07-05 14:33:19 +0530

    Input.java file for using 'Scanner class'

---
+++ sangram/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Input {
+  public static void main(String[] args) {
+    System.out.println("Input");
+    Scanner input =new Scanner(System.in);
+    int a = input.nextInt();
+    System.out.println(a);
+  }
+}
