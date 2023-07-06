f4621235667b194d37491d5e45a47f4e1e50b79d
sangramsingh <banty123a.bksc@outlook.com>
2023-07-06 22:04:22 +0530

    Input.java file updated

--- sangram/Input.java
+++ sangram/Input.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Input {
   public static void main(String[] args) {
-    System.out.println("Input");
+    System.out.print("Input , Your Roll number : ");
     Scanner input =new Scanner(System.in);
     int a = input.nextInt();
-    System.out.println(a);
+    System.out.println("Your Roll number is , "+a+".");
   }
 }
