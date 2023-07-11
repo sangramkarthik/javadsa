649a7dd34c3e38ffba66d23d4542622cacd157d8
sangramsingh <banty123a.bksc@outlook.com>
2023-07-11 16:57:23 +0530

    Circle_area.java file & class file added for calculating Area of Circle

---
+++ sangram/Circle_area.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Circle_area {
+  public static void main(String[] args) {
+    System.out.println("Area of Circle");
+    Scanner input = new Scanner(System.in);
+    System.out.print("Input , the Radius of circle : ");
+    float r = input.nextFloat();
+    System.out.println("The Area of the Circle is "+(r*r*3.14));
+
+
+  }
+}
