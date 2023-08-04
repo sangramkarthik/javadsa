5218bbb1d153113b58e44d2972930ee4821e21f5
sangramkarthik <banty123a.bksc@outlook.com>
2023-08-04 18:29:31 +0530

    All files and folders deleted

--- sangram/Greet.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Greet {
-  public static void main(String[] args) {
-    System.out.print("start , Input your name : ");
-    Scanner input = new Scanner(System.in);
-    String name = input.nextLine();
-    System.out.println("Ram Ram , "+name+"!");
-  }
-}
--- sangram/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Input {
-  public static void main(String[] args) {
-    System.out.print("Input , Your Roll number : ");
-    Scanner input =new Scanner(System.in);
-    int a = input.nextInt();
-    System.out.println("Your Roll number is , "+a+".");
-  }
-}
